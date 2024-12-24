7193d5c790757c81b9a16c8dc95a493403392959
salah <akilsalaheddine203@gmail.com>
2024-12-24 11:49:35 +0100

    add task component and category component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
+import { TaskComponent } from './task/task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "category" , component : CategoryComponent},
+  {path : "task" , component : TaskComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
