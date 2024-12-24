54fc728b9eb74277f6557b71d766a55522ff4466
salah <akilsalaheddine203@gmail.com>
2024-12-24 23:44:11 +0100

    add category crud

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -7,8 +7,34 @@ export class CategoryService {
   private storageKey = 'categories';
 
   constructor() { }
-  
+
   getCategories() : any[] {
     return JSON.parse(localStorage.getItem(this.storageKey) || '[]');
+  }
+
+  saveCategorie(categories : any[]) : void{
+    localStorage.setItem(this.storageKey,JSON.stringify(categories));
+  }
+
+  addCategory(category : any) :void{
+    const categories = this.getCategories();
+    categories.push(category);
+    this.saveCategorie(categories);
+  }
+  updateCategory(updatedCategory : any) : void{
+    const categories = this.getCategories().map(category =>{
+      if(category.id === updatedCategory.id){
+      return updatedCategory
+      }else{
+       return category
+      }
+    });
+    this.saveCategorie(categories);
+  }
+
+  deleteCategory(id:number) : void{
+    const categories = this.getCategories().filter(category => category.id !== id);
+    this.saveCategorie(categories);
+    
   }
 }
