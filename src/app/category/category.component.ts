a446bf1591848f77c994c6060dc6895d45a69b15
salah <akilsalaheddine203@gmail.com>
2024-12-24 23:56:56 +0100

    category service

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'app-category',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrl: './category.component.css'
 })
 export class CategoryComponent {
+  categories : any[] = [];
+  category: any = { id: null, name: '' };
+  constructor(private categoryService : CategoryService){}
+
+  ngOnInit() {
+    this.loadCategories();
+  }
+
+  loadCategories(){
+    this.categories = this.categoryService.getCategories();
+  }
+
+  saveCategory(){
+    if(this.category.id){
+      this.categoryService.updateCategory(this.category);
+    }else{
+      this.categoryService.addCategory(this.category);
+    }
+    this.resetForm();
+    this.loadCategories();
+  }
+
+  deleteCategory(id : number){
+    this.categoryService.deleteCategory(id);
+    this.loadCategories();
+  }
+
+  editCategory(category: any) {
+    this.category = { ...category }; 
+  }
+
+  resetForm() {
+    this.category = { id: null, name: '' };
+  }
+
 
 }
