181b1ac85bafc21a75f43cf9c1eef3afb27228ad
salah <akilsalaheddine203@gmail.com>
2024-12-24 11:57:29 +0100

    add home and navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
 import { CategoryComponent } from './category/category.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskComponent,
-    CategoryComponent
+    CategoryComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
