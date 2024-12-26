3f5e3ff664b3f0ae494f5e7ea656fce2868e523c
salah <akilsalaheddine203@gmail.com>
2024-12-26 15:24:40 +0100

    task service and model

--- src/app/Core/Models/task.model.ts
+++ src/app/Core/Models/task.model.ts
@@ -1,2 +1,21 @@
+export enum Priority {
+    HIGH = 'HAUTE',
+    MEDIUM = 'MOYENNE',
+    LOW = 'BASSE'
+}
+
+export enum Status {
+    COMPLETED = 'TERMINÉ',
+    IN_PROGRESS = 'EN COURS',
+    NOT_STARTED = 'PAS COMMENCÉ'
+}
+
 export class Task {
+    id: number = 0;
+    title?: string;
+    description?: string;
+    dueDate?: Date;
+    priority?: Priority;
+    status?: Status;
+    categoryId?: number; 
 }
--- src/app/Core/Service/task.service.ts
+++ src/app/Core/Service/task.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../Models/task.model';
+import { data } from 'autoprefixer';
+import { take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
+  private storageKey = "task"
   constructor() { }
+
+  getTasks() : Task[]{
+   return JSON.parse(localStorage.getItem(this.storageKey )|| '[]');
+  }
+
+  saveTask(tasks: Task[] ) : void{
+    localStorage.setItem(this.storageKey,JSON.stringify(tasks));
+  }
+
+  addTask(task: Task): void{
+    const taskList = this.getTasks();
+    task.id = Date.now();
+    taskList.push(task);
+    this.saveTask(taskList);
+  }
+
+  updateTask(taskUpdated : Task) : void{
+    const taskList = this.getTasks().map(task => {
+      if(task.id == taskUpdated.id){
+        return taskUpdated
+      }else{
+       return task
+      }
+    });
+    this.saveTask(taskList);
+  }
+
+  deleteTask(id : number):void{
+    const taskList = this.getTasks().filter(task => task.id !== id );
+    this.saveTask(taskList);
+  }
+
 }
